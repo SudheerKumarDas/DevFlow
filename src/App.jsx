6ad803c86a9656a3f11902c45ab99ace422a79cd
SudheerKumarDas <samratsudheerdas@gmail.com>
2026-09-01 16:38:06 +0545

    navbar started

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Dashboard from './pages/Dashboard.jsx'
+
+function App() {
+
+  return (
+    <>
+      <h1>Dev Flow</h1>
+      <Dashboard></Dashboard>
+    </>
+  )
+}
+
+export default App
---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,18 @@
+
+const Dashboard = () => {
+  return (
+    <div>
+        <h1 className="text-3xl">Dashboard</h1>
+        <nav className="flex justify-around items-center">
+            <div className="text-2xl">DEV FLOW</div>
+            <ul className="flex justify-center items-center gap-2">
+                <li>Search</li>
+                <li>Notification</li>
+                <li>Sam</li>
+            </ul>
+        </nav>
+    </div>
+  )
+}
+
+export default Dashboard
